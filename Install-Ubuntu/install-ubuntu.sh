bf93166759a622ae96373ad3974ac6ce0d959205
nicolasbickhoff11 <nicolasbickhoff83@gmail.com>
2025-04-28 08:45:02 -0300

    Update install-ubuntu.sh

--- Install-Ubuntu/install-ubuntu.sh
+++ Install-Ubuntu/install-ubuntu.sh
@@ -1,8 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
+if [ "$(uname -m)" == "aarch64" ]; then
 LINK="https://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-arm64.tar.gz"
+elif [ "$(uname -m)" == "armv7l" ]; then
+LINK="https://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-armhf.tar.gz"
+elif [ "$(uname -m)" == "x86_64" ]; then
+LINK="https://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-amd64.tar.gz"
+else
+echo "Erro: Rootfs não encontrado para essa arquitetura."
+exit 1
+fi
 ROOTFS_PATH="$HOME/ubuntu-fs"
 CACHE_DIR="$HOME/.cache_dir"
-ARCHIVE_NAME="ubuntu-base-22.04-base-arm64.tar.gz"
+ARCHIVE_NAME="ubuntu-base-22.04-base-$(uname -m).tar.gz"
 if [ -d "$ROOTFS_PATH" ]; then
    echo "O Ubuntu já está instalado"
    exit 1
@@ -37,9 +46,10 @@ id | sed 's/.*groups=//' | tr ',' '\n' \
 | grep -v '^c[0-9]\+$' \
 | cut -d' ' -f1 \
 >> $HOME/ubuntu-fs/etc/group
+echo "127.0.0.1 localhost ::1" > $HOME/ubuntu-fs/etc/hosts
 
 unset LD_PRELOAD
-proot -l -0 -r "$HOME/ubuntu-fs" -b /proc -b /dev -b /sys -w /root /usr/bin/env -i HOME=/root TERM=$TERM /bin/bash -c "apt update && apt upgrade -y && apt install sudo vim tzdata wget curl"
+proot -l -0 -r "$HOME/ubuntu-fs" -b /proc -b /dev -b /sys -w /root /usr/bin/env -i HOME=/root TERM=$TERM /bin/bash -c "apt update && apt upgrade -y && apt install sudo vim tzdata wget curl -y"
 chmod +x $HOME/start-ubuntu.sh
 echo "YOU CAN START UBUNTU WITH ~/start-ubuntu.sh"
 exit 1
